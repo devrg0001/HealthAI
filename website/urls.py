7c9124fc3026ceece4b4bf19a09a629726a47122
devrg0001 <dev_gupta@horacemann.org>
2025-01-10 20:30:25 -0500

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+urlpatterns = [
+    path('', views.testing, name='testing'),
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
